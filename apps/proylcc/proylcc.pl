ac30f8f5609d9af3c6a34afbab821eaa0b1663cd
carloza <carlos.i.loza@gmail.com>
2019-05-03 11:16:12 -0300

    detalles sintacticos

--- apps/proylcc/proylcc.pl
+++ apps/proylcc/proylcc.pl
@@ -31,13 +31,13 @@ emptyBoard([
 % RBoard es la configuración resultante de reflejar la movida del jugador Player
 % en la posición Pos a partir de la configuración Board.
 goMove(Board, Color, [Fila,Col], RRBoard):-
-	reemplazarBoard("-", Board, Fila, Col, Color, Board0),
-    cascaraEncerrado(Board, Fila, Col, Color, RBoard).
+	reemplazarBoard("-", Board, Fila, Col, Color, RBoard),
+    cascaraEncerrado(RBoard, Fila, Col, Color, RRBoard).
 
 %reemplazarBoard
-reemplazarBoard(Ant ,Board, Fila, Col, Color, RBoard);-
-	replace(Fila, R, NFila, Board, RBoard),
-    replace(Ant, Col, Color, Fila, NFila).
+reemplazarBoard(Ant ,Board, F, C, Color, RBoard):-
+	replace(Fila, F, NFila, Board, RBoard),
+    replace(Ant, C, Color, Fila, NFila).
 
 % replace(?X, +XIndex, +Y, +Xs, -XsY)
 replace(X, 0, Y, [X|Xs], [Y|Xs]).
@@ -54,7 +54,7 @@ cascaraEncerrado(Board, Fila, Col, Color, RBoard):-
 	ColN is Col-1,
 	ColNN is Col+1,
 	invertirColor(Color,ColorI),
-	cascaraBuscarEncierro(Board0, FilaN, Col, ColorI, Board1),
+	cascaraBuscarEncierro(Board, FilaN, Col, ColorI, Board1),
 	cascaraBuscarEncierro(Board1, Fila, ColN, ColorI, Board2),	
 	cascaraBuscarEncierro(Board2, FilaNN, Col, ColorI, Board3),
 	cascaraBuscarEncierro(Board3, Fila, ColNN, ColorI, RBoard).
@@ -71,17 +71,17 @@ cascaraBuscarEncerrado(Board, Fila, Col, Color, RBoard):-
 
 
 %estos son los caso donde me caigo del Tablero, no deberia moficar el Board
-limpiarEncerrado(Board, -1, Col, Color, Board).
-limpiarEncerrado(Board, Fila, -1, Color, Board).
-limpiarEncerrado(Board, 19, Col, Color, Board).
-limpiarEncerrado(Board, Fila, 19, Color, Board).
+limpiarEncerrado(Board, -1, _Col, _Color, Board).
+limpiarEncerrado(Board, _Fila, -1, _Color, Board).
+limpiarEncerrado(Board, 19, _Col, _Color, Board).
+limpiarEncerrado(Board, _Fila, 19, _Color, Board).
 %este es otro caso donde el color de la ficha es de otro color al que estoy limpiando
 limpiarEncerrado(Board, Fila, Col, Color, Board):-
 	invertirColor(Color, ColorI),
 	reemplazarBoard(ColorI, Board, Fila, Col, ColorI, Board).
 %caso general
-limpiarEncerrado(Board, Fila, Col, Color, Rta):-
-	noVacio(Board, Fila, Col)), %aca verifico que esa celda no es vacia (si fuera vacia no estaria encerrada)
+limpiarEncerrado(Board, Fila, Col, Color, RBoard):-
+	noVacio(Board, Fila, Col), %aca verifico que esa celda no es vacia (si fuera vacia no estaria encerrada)
 	%reemplazarBoard( _, Board, Fila, Col, Color, Board), %miro que sea el color que venia limpiando
 	reemplazarBoard(Color, Board, Fila, Col, "-", Board0), %aca efectivamente la limpio (es decir coloco un vacio)
 														   %tambien verificaria si es del color que venia cambiando
