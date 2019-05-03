9159c3b76f62a9db2a7d3097abcab07b71144eb4
Javier <31253413+JavierTierno@users.noreply.github.com>
2019-05-02 23:35:06 -0300

    Modularizando
    Creado el metodo encerrado. (Sin terminar)

--- apps/proylcc/proylcc.pl
+++ apps/proylcc/proylcc.pl
@@ -3,7 +3,7 @@
 		emptyBoard/1,
 		goMove/4
 	]).
-	
+% Empiezo a contar de 0 hasta 18 (19 filas x 19 columnas).
 emptyBoard([
 		 ["-","-","-"]]).
 /*
@@ -37,7 +37,7 @@ emptyBoard([
 % en la posición Pos a partir de la configuración Board.
 
 goMove(Board, Player, [R,C], RBoard):-
-    replace(Row, R, NRow, Board, RBoard),
+	replace(Row, R, NRow, Board, RBoard),
     replace("-", C, Player, Row, NRow).
 
 
@@ -53,16 +53,60 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
     XIndexS is XIndex - 1,
     replace(X, XIndexS, Y, Xs, XsY).
 	
-	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-% encerrado(Tablero, Fila, Col, Color, Lista?(Para evitar ciclos).
+% encerrado(+Tablero, +Fila, +Col, +Color, -Lista?(Para evitar ciclos).
 %
-	
-%encerrado(Board, Fila, Col, Player):-
 
-	
+encerrado(Board, Fila, Col, Color, Lista):-
+	FilaN is Fila-1,
+	FilaNN is Fila+1,
+	ColN is Col-1,
+	ColNN is Col+1,
+	buscarEncerrado(Board, FilaN, Col, Color, Lista1),
+	buscarEncerrado(Board, Fila, ColN, Color, Lista2),	
+	buscarEncerrado(Board, FilaNN, Col, Color, Lista3),
+	buscarEncerrado(Board, Fila, ColNN, Color, Lista4),
+	borrarTablero(Lista1),
+	borrarTablero(Lista2),
+	borrarTablero(Lista3),
+	borrarTablero(Lista4).
+		
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% buscarEncerrado
+%
+buscarEncerrado(Board, -1, Col, Color, []).
 
+buscarEncerrado(Board, Fila, -1, Color, []).
+
+buscarEncerrado(Board, 19, Col, Color, []).
+
+buscarEncerrado(Board, Fila, 19, Color, []).
+
+buscarEncerrado(Board, Fila, Col, Color, []):-
+	verVacio(Board, Fila, Col).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% verVacio
+%	
+	verVacio(Board, Fila, Col):-
+		buscar(Board, Fila, NFila),
+		buscar(NFila, Col, "-").
+		
+		
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% buscar
+%	
+	buscar([L|Ls], 0, L).
+	buscar([L|Ls], N, Rta):-
+		NN is N-1,
+		buscar(Ls, NN, Rta).
+	
+	
 	
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
